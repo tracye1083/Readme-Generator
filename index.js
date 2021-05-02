3ffc33ae9a6826eefc203ed271b1b80b9f0cee92
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-05-02 17:59:52 -0400

    repo reorg
    moved files so node would function appropriately

---
+++ index.js
@@ -0,0 +1,73 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+inquirer.prompt([{
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project: ',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions: ',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information: ',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Select a license',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines: ',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Enter testing instructions: ',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub Username?',
+        name: 'questionsUser',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'questionsEmail',
+    },
+])
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+.then((response) => {
+    console.log(response)
+    let { fname, location, hobby, github, linkedin } = response;
+    fs.writeFile("README.md", `
+
+# Title
+${response.title}
+`, "utf8", (err) => err ? console.error(err) : console.log('Success!'));
+});
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
